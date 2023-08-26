ae923faa2fc02e00501a82173110d016c3dd1e7f
edilsossilva1976 <edilsossilva@gmail.com>
2023-08-26 13:44:03 -0300

    commit inicial

---
+++ scripts-js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#email').mask('user@youremail.com');
+    $('#fone').mask('(61) 9.0000-0000');
+    $('#cpf').mask('000.000.000-00');
+    $('#cep').mask('00.000-000');
+})
